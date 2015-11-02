4e4ca4d7c15976797d5e5d54a523aa951c86b0c1
Michael Freeman <michael.k.freeman@gmail.com>
2015-11-02 08:28:00 -0800

    finished first class

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 // Create an angular application called "sortApp"
+var sortApp = angular.module('sortApp', []);
 
+sortApp.controller('mainController', function($scope, $http){
+	$scope.sortType = 'name'
+	$http.get('data/salaries.json').then(function(response) {
+		$scope.data = response.data		
+		console.log($scope.data)
+	})
+})
 
 // Define a controller "mainController" for you application, 
 // making sure to pass in both the $scope and $http variables
