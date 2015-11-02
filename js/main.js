7f66dc80640f0a7c257d76114f4902c6ae3db208
Yi Pang <pangyibobo@gmail.com>
2015-11-02 10:15:02 -0800

    class

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var sortApp = angular.module('sortApp', []);
 
 sortApp.controller('mainController', function($scope, $http){
 	$scope.sortType = 'name'
-	$http.get('data/salaries.json').then(function(response) {
+    	$http.get('data/salaries.json').then(function(response) {
 		$scope.data = response.data		
 		console.log($scope.data)
 	})
